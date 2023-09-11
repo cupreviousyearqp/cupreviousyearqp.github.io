d926eb201b4cdca109fe27d7a263c774f6347213
cupreviousyearqp <CUQPapers@gmail.com>
2023-09-11 21:54:57 +0530

    UPDATE v3 FINAL

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
 function changeSemesterGuide(){
   var selectBoxValue = document.getElementById("selectBox").value;
-  var cover = document.getElementById("container-cover");
+
+  var semester = document.getElementById("semester");
+
+  var item7 = document.getElementById("sem-item-7th");
+  var item8 = document.getElementById("sem-item-8th");
 
   if(selectBoxValue == "BA")
   {
-      console.log("test");
-      cover.style.down = "1000px";
+      console.log("BA");
+      item7.style.opacity = "0";
+      item8.style.opacity = "0";
+      semester.style.marginBottom = "-700px";
   }
 
   else if(selectBoxValue == "BBA")
   {
-      console.log("test");
-      cover.style.down = "1000px";
+      console.log("BBA");
+      item7.style.opacity = "0";
+      item8.style.opacity = "0";
+      semester.style.marginBottom = "-700px";
   }
 
   else if(selectBoxValue == "BCA")
   {
-    console.log("test");
+    console.log("BCA");
+    item7.style.opacity = "1";
+    item8.style.opacity = "1";
+    semester.style.marginBottom = "-350px";
   }
 
   else if(selectBoxValue == "BSC")
   {
-    console.log("test");
+    console.log("BSC");
+    item7.style.opacity = "1";
+    item8.style.opacity = "1";
+    semester.style.marginBottom = "-350px";
   }
 
   else if(selectBoxValue == "BCOM")
   {
-      console.log("test");
+      console.log("BCOM");
+      item7.style.opacity = "0";
+      item8.style.opacity = "0";
+      semester.style.marginBottom = "-700px";
   }
 
   else if(selectBoxValue == "MCOM")
   {
-    console.log("test");
+    console.log("MCOM");
+    item7.style.opacity = "0";
+    item8.style.opacity = "0";
+    semester.style.marginBottom = "-700px";
   }
 
   else
   {
-      console.log("test");
+      console.log("MA");
+      item7.style.opacity = "0";
+      item8.style.opacity = "0";
+      semester.style.marginBottom = "-700px";
   }
 }
