2291be0118277e920de7fe7cdb5f9faad399cc78
cupreviousyearqp <CUQPapers@gmail.com>
2023-09-14 01:27:32 +0530

    update v7
    the select boxes are setup

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function changeSemesterGuide(){
   var semester = {};
   semester["BA"] = ["Semester 1", "Semester 2", "Semester 3", "Semester 4", "Semester 5", "Semester 6"];
@@ -27,6 +25,31 @@ function changeSemesterGuide(){
     for (i = 0; i < semesterBoxObj.length; i++) {
       var option = new Option(semesterBoxObj[i], i);
       sem.options.add(option);
+    }
+  }
+}
+
+function changeSubjectGuide(){
+  var subject = {};
+
+
+  var sem = document.getElementById("semesterSelectBox");
+  var sub = document.getElementById("subjectSelectBox");
+  var semIndex = sem.options[sem.selectedIndex].value;
+
+  document.getElementById("semester").style.paddingBottom = "50px";
+  document.getElementById("subject").style.opacity = "1";
+
+  while (sub.options.length) {
+    sub.remove(0);
+  }
+
+  var subjectBoxObj = subject[semIndex];
+  if (subjectBoxObj) {
+    var i;
+    for (i = 0; i < subjectBoxObj.length; i++) {
+      var option = new Option(subjectBoxObj[i], i);
+      sub.options.add(option);
     }
   }
 }
